d89f58ed99de4652fe458037bd01a65c2e070d9e
Jiayu Liu <jimexist@gmail.com>
2016-01-02 00:11:25 +0800

    add styles.css

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './styles.css';
+
+ReactDOM.render(
+  <div className='container'></div>,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path         = require('path');
+var autoprefixer = require('autoprefixer');
+var precss       = require('precss');
+var atImport     = require('postcss-import');
+
+var config = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: "[name].js"
+  },
+  postcss: function() {
+    return [atImport, autoprefixer, precss];
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        query: {
+          presets: ['react', 'es2015']
+        },
+        loader: 'babel'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css?localIdentName=[name]-[local]-[hash:base64:5]!postcss'
+      },
+      {
+        test: /\.png$/,
+        loader: "url-loader?limit=100000"
+      },
+      {
+        test: /\.jpg$/,
+        loader: "file-loader"
+      }
+    ]
+  }
+};
+
+module.exports = config;
